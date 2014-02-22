613185ff3908645109fa4e220267ce8c9208795a
Hamuko <hamuko@burakku.com>
2014-02-22 16:08:47 +0200

    Support for more than two duplicate chapters. duplicate_chapters() will now be able to resolve more than two duplicate chapters, should such a need arise (I really doubt it will). Also, print_info() now has an optional argument called 'newline', which defaults to True and if set to False, will not shift to the next line after printing the message.

--- Manager.py
+++ Manager.py
@@ -8,9 +8,12 @@ import urllib.request
 import zipfile
 from Batoto import Batoto
 
-def print_info(message):
+def print_info(message, newline=True):
 	if not 'silent_mode' in globals():
-		print(message)
+		if newline == False:
+			print(message, end="")
+		else:
+			print(message)
 
 def download_file(url, filename):
 	file_extension = re.search(r'.*\.(.*)', url).group(1)
@@ -40,49 +43,67 @@ def zip_files(filelist, filename):
 	print_info("Zip created: " + filename)
 
 def duplicate_chapters(chapters):
+	numbers = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
+
+	def print_initial():
+		print_info("{} releases for chapter {}: ".format(numbers[len(duplicates)], duplicates[0]["chapter"]), newline=False)
+		for item in duplicates[:-1]:
+			print_info("{}, ".format(item["group"]), newline=False)
+		print_info("{}.".format(duplicates[-1]["group"]))
+
 	def no_preference():
-		print_info("Two releases for chapter {}: {} and {}.".format(chapter["chapter"], chapter["group"], chapter2["group"]))
-		print_info("No preference set. Picking {} for chapter {}.".format(chapter["group"], chapter["chapter"]))
-		chapters.remove(chapter2)
+		print_initial()
+
+		print_info("No preference set. Picking {} for chapter {}.".format(duplicates[-1]["group"], chapter["chapter"]))
+		for item in duplicates[:-1]:
+			chapters.remove(item)
 
 	def preference(group):
-		print_info("Two releases for chapter {}: {} and {}.".format(chapter["chapter"], chapter["group"], chapter2["group"]))
-		if chapter["group"] == group:
-			print_info("Preference: {}. Picking {} for chapter {}.".format(group, chapter["group"], chapter["chapter"]))
-			chapters.remove(chapter2)
-		elif chapter2["group"] == group:
-			print_info("Preference: {}. Picking {} for chapter {}.".format(group, chapter2["group"], chapter2["chapter"]))
-			chapters.remove(chapter)
-		else:
-			print_info("Preference: {}. Not found. Picking {} for chapter {}.".format(group, chapter["group"], chapter["chapter"]))
-			chapters.remove(chapter2)
+		print_initial()
+
+		for item in duplicates:
+			if item["group"] == group:
+				print_info("Preference: {}. Picking {} for chapter {}.".format(group, item["group"], item["chapter"]))
+				duplicates.remove(item)
+				for item in duplicates:
+					chapters.remove(item)
+				return
+
+		print_info("Preference: {}. Not found. Picking {} for chapter {}.".format(group, duplicates[-1]["group"], duplicates[-1]["chapter"]))
+		for item in duplicates[:-1]:
+			chapters.remove(item)
 
 	def interactive():
-		print_info("Two releases for chapter {}: {} and {}.".format(chapter["chapter"], chapter["group"], chapter2["group"]))
-		print("1. {}".format(chapter["group"]))
-		print("2. {}".format(chapter2["group"]))
+		print_initial()
+		for num, item in enumerate(duplicates, start=1):
+			print("{}. {}".format(num, item["group"]))
+
+		# Try to delete the given item from the duplicates list. Loops until a valid item is entered.
 		while(True):
 			choice = input('>> ')
-			if choice == '1':
-				print_info("Picking {} for chapter {}.".format(chapter["group"], chapter["chapter"]))
-				chapters.remove(chapter2)
+			try:
+				print_info("Picking {} for chapter {}.".format(duplicates[int(choice)-1]["group"], duplicates[int(choice)-1]["chapter"]))
+				del duplicates[int(choice)-1]
 				break
-			elif choice == '2':
-				print_info("Picking {} for chapter {}.".format(chapter2["group"], chapter2["chapter"]))
-				chapters.remove(chapter)
-				break
-			else:
+			except:
 				print("Invalid input.")
 
+		# Deletes all the chapters that are in the duplicates list from the chapter list, since the version to keep is no longer on that list.
+		for item in duplicates:
+			chapters.remove(item)
+
 	for num, chapter in enumerate(chapters):
-			for chapter2 in chapters[num+1:]:
-				if chapter["chapter"] == chapter2["chapter"]:
-					if 'interactive_mode' in globals():
-						interactive()
-					elif 'group_preference' in globals():
-						preference(group_preference)
-					else:
-						no_preference()
+		duplicates = [chapter]
+		for chapter2 in chapters[num+1:]:
+			if chapter["chapter"] == chapter2["chapter"]:
+				duplicates.append(chapter2)
+		if len(duplicates) > 1:
+			if 'interactive_mode' in globals():
+				interactive()
+			elif 'group_preference' in globals():
+				preference(group_preference)
+			else:
+				no_preference()
 
 # Split the given arguments into options and arguments.
 optlist, args = getopt.getopt(sys.argv[1:], 'qs:e:', ['prefer-group=', 'interactive'])
